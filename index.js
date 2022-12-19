628164d9a211d4c60baf2c0ed8ecf1d272d81a16
jjyy0804 <jjyy0804@naver.com>
2022-12-20 02:28:30 +0900

    비밀정보다

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser=require('body-parser');
+const {User} = require("./models/User");
+const config = require("./config/key");
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+
+//applicaion/json
+app.use(bodyParser.json());
+
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://jjyy0804:root1234@cluster0.n8ygwdt.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI)
   .then(()=>console.log('MongoDB Connected...'))
   .catch(err=>console.log(err))
 
 
-app.get('/',(req,res)=>res.send('Hello World!'))
+app.get('/',(req,res)=>res.send('Hello World! 이리온'))
+
+app.post('/register',(req,res)=>{
+
+  //회원가입 시 필요한 정보들을 클라이언트에서 가져오면
+  //그것들을 데이터베이스에 넣어준다.
+
+    const user=new User(req.body)
+
+    user.save((err,userInfo)=>{
+      if(err) return res.json({success:false,err})
+      return res.status(200).json({
+        success:true
+      })
+    })
+
+})
 
 app.listen(port,()=>console.log(`Example app listening on port ${port}!`))
